438458cba8b33400f543f632b02228d31d7a00e4
Yash Hegde <yash4success@gmail.com>
2024-06-26 08:07:37 +0530

    init+schema generated+signin route created

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+// Create the main Hono app
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>();
+
+app.post('/api/v1/signup', async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  try {
+    await prisma.user.create({
+    data: {
+      username: body.username,
+      password: body.password,
+      name:body.name,
+    }
+  })
+
+	return c.text('user created')  
+  } catch {
+    c.status(411)
+   return c.text('error during user creation ')   
+  }
+  
+})
+
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+	const id = c.req.param('id')
+	console.log(id);
+	return c.text('get blog route')
+})
+
+app.post('/api/v1/blog', (c) => {
+
+	return c.text('signin route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('signin route')
+})
+
+export default app;
+

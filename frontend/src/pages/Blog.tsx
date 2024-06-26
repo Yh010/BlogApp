35934c25bcad8363640e52264b9486d6fb4b3acb
Yash Hegde <yash4success@gmail.com>
2024-06-26 20:02:49 +0530

    blog page

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,3 +1,21 @@
 export const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="flex h-screen">
+      <div className="w-3/4 border p-6">
+        <div>Blog Title</div>
+        <div>Blog posted date</div>
+        <div>blog desc</div>
+      </div>
+      <div className="w-1/4 p-6">
+        <div className="border-b-2">Author</div>
+        <div className="flex">
+          <div className="m-2">Author Pic</div>
+          <div>
+            <div>Author Name</div>
+            <div>Author Desc</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };

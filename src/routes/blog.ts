c73417d8bcebd71637e450384cff719094eb75d2
Yash Hegde <yash4success@gmail.com>
2024-06-26 09:47:11 +0530

    blog routes, and creating better routing

---
+++ src/routes/blog.ts
@@ -0,0 +1,104 @@
+import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+export const blogRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string
+  }
+}>();
+
+blogRouter.use("/*", (c, next) => {
+    next()
+})
+blogRouter.get('/', async (c) => {
+	const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    try {
+        const blog = await prisma.blog.findFirst({
+            where: {
+                id: body.id
+            },
+        })
+
+        return c.json({
+            blog: blog
+        })
+    } catch (e) {
+        return c.json({
+           message:"error while fetching the blog"
+        })
+    }
+    
+})
+
+blogRouter.get('/bulk', async (c) => {
+	const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const blogs = await prisma.blog.findMany()
+
+    return c.json({
+        blogs:blogs
+    })
+})
+
+blogRouter.post('/', async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    try {
+        const blog = await prisma.blog.create({
+            data: {
+                title: body.title,
+                content: body.content,
+                authorId: 1,
+            }
+        })
+
+        return c.json({
+            id:blog.id
+        })    
+    } catch (e) {
+        return c.json({
+           message:"error while posting the blog"
+        })
+    }
+    
+})
+
+blogRouter.put('/', async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    try {
+        const blog = await prisma.blog.update({
+            where: {
+                id: body.id
+            },
+            data: {
+                title: body.title,
+                content: body.content,
+            }
+        })
+
+        return c.json({
+            id:blog.id
+        })
+	    
+    } catch (e) {
+        return c.json({
+           message:"error while updating the blog"
+        })
+    }
+    
+})

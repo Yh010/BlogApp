d0fb66b1f45d53c8c08eba4a912be61993d19799
Yash Hegde <yash4success@gmail.com>
2024-06-26 14:37:16 +0530

    zod validation

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt';
+import { signupInput } from "../zod";
 export const userRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
@@ -11,6 +12,13 @@ export const userRouter = new Hono<{
 
 userRouter.post('/signup', async (c) => {
   const body = await c.req.json();
+  const {success} = signupInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "invalid inputs"
+    })
+  }
   const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())

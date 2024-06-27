4225662c09fa3edffebf58b70559951ea3b24c10
Yash Hegde <yash4success@gmail.com>
2024-06-27 12:29:17 +0530

    taking user inputs in signin and signup form

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
 import { InputBox } from "../components/signupComponents/inputBox";
 import { SignUpButton } from "../components/signupComponents/SignupButton";
+import { SigninInput } from "@yh010/blogapp-common";
 
 export const Signin = () => {
+  const [postInputs, setpostInputs] = useState<SigninInput>({
+    username: "",
+    password: "",
+    name: "",
+  });
+
+  const handleChange = (field: keyof SigninInput, value: string) => {
+    setpostInputs({ ...postInputs, [field]: value });
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="border p-6 rounded-lg">
-        <InputBox placeholder="Email address" />
-        <InputBox placeholder="Password" />
+        <InputBox
+          placeholder="Username"
+          cb={(value) => handleChange("username", value)}
+          prev={postInputs.username}
+        />
+        <InputBox
+          placeholder="Password"
+          cb={(value) => handleChange("password", value)}
+          prev={postInputs.password}
+        />
+        <InputBox
+          placeholder="Name"
+          cb={(value) => handleChange("name", value)}
+          prev={postInputs.name || ""}
+        />
         <SignUpButton text="Sign In" />
       </div>
+      {JSON.stringify(postInputs)}
     </div>
   );
 };
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,14 +1,40 @@
+import { SignupInput } from "@yh010/blogapp-common";
 import { InputBox } from "../components/signupComponents/inputBox";
 import { SignUpButton } from "../components/signupComponents/SignupButton";
+import { useState } from "react";
 
 export const Signup = () => {
+  const [postInputs, setpostInputs] = useState<SignupInput>({
+    username: "",
+    password: "",
+    name: "",
+  });
+
+  const handleChange = (field: keyof SignupInput, value: string) => {
+    setpostInputs({ ...postInputs, [field]: value });
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="border p-6 rounded-lg">
-        <InputBox placeholder="Email address" />
-        <InputBox placeholder="Password" />
+        <InputBox
+          placeholder="Username"
+          cb={(value) => handleChange("username", value)}
+          prev={postInputs.username}
+        />
+        <InputBox
+          placeholder="Password"
+          cb={(value) => handleChange("password", value)}
+          prev={postInputs.password}
+        />
+        <InputBox
+          placeholder="Name"
+          cb={(value) => handleChange("name", value)}
+          prev={postInputs.name || ""}
+        />
         <SignUpButton text="Sign Up" />
       </div>
+      {JSON.stringify(postInputs)}
     </div>
   );
 };

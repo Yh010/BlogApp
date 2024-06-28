c3d560917f3842e5a0550ba82f7088468c14ff83
Yash Hegde <yash4success@gmail.com>
2024-06-28 09:26:31 +0530

    publish new blog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { Blog } from "./pages/Blog";
 import { Signin } from "./pages/SignIn";
 import { Signup } from "./pages/SignUp";
 import { AllBlogs } from "./pages/AllBlogs";
+import { PublishBlog } from "./pages/PublishBlog";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/blogs" element={<AllBlogs />} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/blogs" element={<AllBlogs />} />
+        <Route path="/publish" element={<PublishBlog />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.jpg";
 import { PublishButton } from "./PublishButton";
 import { UserProfileIcon } from "./UserProfile/UserProfileIcon";
@@ -5,15 +6,19 @@ export const Navbar = () => {
   return (
     <div className="border-b-2 flex justify-between p-4">
       <div className="flex justify items-center space-x-2">
-        <div>
-          <img className="h-10 w-10 rounded-full" src={logo} />
-        </div>
-        <div>Blog App</div>
+        <Link to={"/blogs"}>
+          <div>
+            <img className="h-10 w-10 rounded-full" src={logo} />
+          </div>
+          <div>Blog App</div>
+        </Link>
       </div>
 
       <div className="flex justify items-center space-x-4">
         <div>
-          <PublishButton />
+          <Link to={"/publish"}>
+            <PublishButton />
+          </Link>
         </div>
         <div>
           <UserProfileIcon inputname="users Name" />

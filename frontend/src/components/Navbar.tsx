a65d8a96ce6cfc8abb52406511293bdabe6af8ca
Yash Hegde <yash4success@gmail.com>
2024-06-29 18:22:49 +0530

    socials added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import logo from "../assets/logo.jpg";
 import { PublishButton } from "./PublishButton";
 import { UserProfileIcon } from "./UserProfile/UserProfileIcon";
+import github from "../assets/github.png";
+import linkedin from "../assets/linkedin.png";
+import twitter from "../assets/twitter.png";
 export const Navbar = () => {
   return (
     <div className="bg-amber-100 border-b-2 border-black flex justify-between p-4">
@@ -11,7 +14,35 @@ export const Navbar = () => {
         </div>
         <div>Blog App</div>
       </Link>
-      <div className="flex justify items-center">Made by Yash Hegde</div>
+      <div className="flex justify items-center space-x-2">
+        <div>Made by Yash Hegde</div>
+        <div className="flex space-x-4 justify items-center">
+          <div>
+            <a
+              className="flex space-x-4 items-center"
+              href="https://github.com/Yh010/"
+            >
+              <img className="h-8 w-8" src={github} />
+            </a>
+          </div>
+          <div>
+            <a
+              className="flex space-x-4 items-center"
+              href="https://www.linkedin.com/in/yash-hegde-927721201/"
+            >
+              <img className="h-8 w-8" src={linkedin} />
+            </a>
+          </div>
+          <div>
+            <a
+              className="flex space-x-4 items-center"
+              href="https://x.com/YashHegde7"
+            >
+              <img className="h-8 w-8" src={twitter} />
+            </a>
+          </div>
+        </div>
+      </div>
 
       <div className="flex justify items-center space-x-4">
         <div>
